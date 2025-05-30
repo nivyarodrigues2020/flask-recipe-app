d776bf936ce02bf860b8ed32b5fdc6007166dc99
nivyarodrigues2020 <nivyarodrigues1997@gmail.com>
2025-05-30 03:21:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -180,5 +180,8 @@ def chat():
 # -----------------------------
 # RUN THE APP
 # -----------------------------
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
